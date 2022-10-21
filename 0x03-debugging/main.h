e33d5e8f6e3c733ab91216b88a1643ee03a8dbaf
GCremez <salauolorunjuealo@gmail.com>
2022-10-21 07:40:56 -0700

    Main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,10 +1,16 @@
-#ifndef main.h
-#define main.h
-#include <stdio.h>
+#ifndef HEADER_FILE
+
+#define HEADER_FILE
+
+int _putchar(char c);
+
 
 void positive_or_negative(int i);
+
 int largest_number(int a, int b, int c);
+
 void print_remaining_days(int month, int day, int year);
+
 int convert_day(int month, int day);
 
-#endif /* main_h */
+#endif
